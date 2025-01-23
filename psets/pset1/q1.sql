f1b942d83ace6a9ad2d6822c44b5efa60b1edab1
flor-morada <florMorada@protonmail.com>
2025-01-23 13:15:28 -0700

    wip

--- psets/pset1/q1.sql
+++ psets/pset1/q1.sql
@@ -2,7 +2,7 @@
 -- Your query goes here.
 SELECT station_id || '|' || route_id || '|' || distance_from_last_station_miles
 FROM station_orders 
-WHERE distance_from_last_station_miles > 1 
+WHERE distance_from_last_station_miles >= 1 
 ORDER BY 
     distance_from_last_station_miles DESC,
     route_id ASC, 
