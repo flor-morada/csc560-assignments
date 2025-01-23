92bb50c4d30df1e9731396e0244492ab2a55a290
flor-morada <florMorada@protonmail.com>
2025-01-23 13:35:51 -0700

    wip: q3

--- psets/pset1/q3.sql
+++ psets/pset1/q3.sql
@@ -1,2 +1,12 @@
 -- Q3
 -- Your query goes here.
+select season || '|' || line_id || '|' || direction || '|' ||  total_ons 
+from rail_ridership 
+where 
+    time_period_id='time_period_06' 
+    and station_id=(
+        select station_id from stations where station_name='Kendall/MIT'
+    )
+order by
+    season asc,
+    direction asc;
