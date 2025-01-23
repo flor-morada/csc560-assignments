0a13c2514993e895b3fe852bcf6f6c8d1d22c586
fedfed2 <fede.ben.fernandez@gmail.com>
2025-01-23 13:14:56 -0700

    Pipes in Q2

--- psets/pset1/q2.sql
+++ psets/pset1/q2.sql
@@ -1,5 +1,5 @@
 -- Q2
 -- Your query goes here.
-select line_id, direction_desc, a.station_name, b.station_name
+select line_id || '|' || direction_desc || '|' || a.station_name || '|' || b.station_name
 from (routes join stations as a on (first_station_id=a.station_id)) join stations as b on (last_station_id=b.station_id)
 order by line_id, direction_desc, a.station_name, b.station_name;
