9ea55060f5f01ad5da20b788de108eb1f6e322aa
fedfed2 <fede.ben.fernandez@gmail.com>
2025-01-23 13:33:18 -0700

    Q4

--- psets/pset1/q4.sql
+++ psets/pset1/q4.sql
@@ -1,2 +1,7 @@
 -- Q4
 -- Your query goes here.
+select routes.route_id || '|' || direction || '|' || route_name || '|' || count(station_id) || '|' || sum(distance_from_last_station_miles)
+    from routes join station_orders on routes.route_id=station_orders.route_id
+    where line_id <> 'green'
+    group by routes.route_id
+    order by count(station_id) DESC, sum(distance_from_last_station_miles) DESC;
