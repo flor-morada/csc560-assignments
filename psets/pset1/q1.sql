09692c2c2d2fc370544b0963625e1915ea0d3250
flor-morada <florMorada@protonmail.com>
2025-01-23 12:56:06 -0700

    initial commit

--- psets/pset1/q1.sql
+++ psets/pset1/q1.sql
@@ -1,2 +1,9 @@
 -- Q1
 -- Your query goes here.
+SELECT station_id || '|' || route_id || '|' || distance_from_last_station_miles
+FROM station_orders 
+WHERE distance_from_last_station_miles > 1 
+ORDER BY 
+    distance_from_last_station_miles DESC,
+    route_id ASC, 
+    station_id ASC; 
