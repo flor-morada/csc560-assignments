0e71aab87030dd5b53ba98eb37cc271ed06a5f05
fedfed2 <fede.ben.fernandez@gmail.com>
2025-01-27 14:23:18 -0700

    Q6 and Q8

--- psets/pset1/q6.sql
+++ psets/pset1/q6.sql
@@ -1,2 +1,7 @@
 -- Q6
 -- Your query goes here.
+select station_name, max(cnt) from 
+    (select station_name, count(gated_entries) as cnt
+    from stations join gated_station_entries on stations.station_id=gated_station_entries.station_id
+    where service_date like '2021-06%' or service_date like '2021-07%' or service_date like '2021-08%'
+    group by stations.station_id);
--- psets/pset1/q8.sql
+++ psets/pset1/q8.sql
@@ -1,2 +1,8 @@
 -- Q8
 -- Your query goes here.
+select station_name, total_ons from
+    stations join rail_ridership on stations.station_id = rail_ridership.station_id
+    where line_id = 'orange' and season = 'Fall 2018' and time_period_id = 'time_period_01' and direction = 0
+    and total_ons > (select avg(total_ons) from rail_ridership
+    where line_id = 'orange' and season = 'Fall 2018' and time_period_id = 'time_period_01' and direction = 0)
+    order by total_ons DESC, station_name;
