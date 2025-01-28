ab01a5dedf0a187a798ce398308ab294576fdd98
fedfed2 <fede.ben.fernandez@gmail.com>
2025-01-27 19:56:10 -0700

    Q10 first trygit add .

--- psets/pset1/q10.sql
+++ psets/pset1/q10.sql
@@ -1,2 +1,9 @@
 -- Q10
 -- Your query goes here.
+select station_name || '|' || line_name || '|' || max(bypass) from
+    (select station_name, line_name, (cast(sum(average_flow) as real)-(sum(average_ons)+sum(average_offs)))/cast(sum(average_flow) as real) as bypass from
+    rail_ridership join stations on stations.station_id = rail_ridership.station_id join lines on rail_ridership.line_id = lines.line_id
+    where season = 'Fall 2019'
+    group by rail_ridership.line_id, rail_ridership.station_id)
+    group by line_name
+    order by line_name ASC;
